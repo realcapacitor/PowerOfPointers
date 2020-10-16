590d04044076e71d57e80744e57966d95dde7034
Shivam Kishor Patange <71215306+realcapacitor@users.noreply.github.com>
2020-10-16 23:15:47 +0530

    Update PowerOfPointers.c

--- PowerOfPointers.c
+++ PowerOfPointers.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
+
+
+
 typedef struct{
 	char name[15];
 	long num;
 }con;
 
-
 int main(){
     //Power of Pointers
 	//Creating database: Contacts
+		
 		con c1;
 
 		char* ptrc=&c1.name;
 		long* ptri=&c1.num;
-
-
 		int v=0;
+		int z=0;
+	
 		for(int w=0;w<120;w=w+24){
 			printf("Enter name: ");
 			scanf("%s",(ptrc+w));
@@ -22,13 +25,12 @@ int main(){
 			scanf("%ld",(ptri+v));
 			v=v+3;
 		}
-		int z=0;
+		
 		for(int yo=0;yo<120;yo=yo+24){
 			printf("Name: %s\t",(ptrc+yo));
 			printf("Number: %ld\n",*(ptri+z));
 			z=z+3;
 		}
 
-    
     return 0;
 }
